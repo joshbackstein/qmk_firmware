00a6c279d82d4d8d874fa00e6a0a15c7ee37e2cc
joshbackstein <josh.backstein@gmail.com>
2017-09-28 14:23:12 -0600

    Renamed DEFAULT layer to QWERTY.

--- keyboards/atreus62/keymaps/joshbackstein/keymap.c
+++ keyboards/atreus62/keymaps/joshbackstein/keymap.c
@@ -31,12 +31,15 @@ enum custom_keycodes {
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _DEFAULT 0
+#define _QWERTY 0
 #define _FN 1
 #define _LAYER_STOP 2
 #define _LAYERS 3
 #define _NAV 4
 #define _RESET 5
+
+// Define default layer
+#define _DEFAULT _QWERTY
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 */
 
-[_DEFAULT] = { // qwerty
+[_QWERTY] = { // qwerty
 		{ KC_GRV,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_TRNS,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MUTE },
 		{ KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_TRNS,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS },
 		{ MO(_FN),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_TRNS,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
